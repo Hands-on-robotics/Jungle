f5d12646257b5437599fdb76ff8ebf04ccc8421d
Hands-on-robotics <handsonevanoff@gmail.com>
2024-02-08 19:00:18 -0800

    add product and category counts to dashboard

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,7 +1,25 @@
+# Feature: Admin Dashboard Counts
+# Instruction
+# Implement the following feature:
+
+# The admin dashboard page is currently just a place holder, providing no value to admins.
+
+# Replace the text on the dashboard page with these general stats about the store:
+
+# Display a count of how many products are in the database
+# Display a count of how many categories are in the database
+# Tips
+# We could use AR models to query the database directly from the ERB views. However a better, more MVC approach to this is to let the controller fetch the data and pass it to the template. In other words, set instance variables in the controller action with values, and then render those instance variables in the ERB template
+# Stretch
+# Try to make it look nice, using Bootstrap 5 elements.
+# Link to the product admin list/index pages when displaying the counts.
+
 class Admin::DashboardController < ApplicationController
   before_action :authenticate
 
   def show
+    @products_count = Product.count
+    @categories_count = Category.count
   end
 
   private
